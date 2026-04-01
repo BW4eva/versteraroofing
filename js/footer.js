43de742e072a028e9ea8610fa8a26d5e81d872cf
BW4eva <calvin.robe@gmail.com>
2026-04-01 15:22:50 -0500

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -19,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 <div class="footer-column">
                     <h3>Locations</h3>
                     <ul>
-                        <li><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109907.85808762973!2d-97.88540530273437!3d30.588255000000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xe8378ec716b107%3A0xad97f3a517d3a80b!2sGreen%20Shield%20Roofing!5e0!3m2!1sen!2sus!4v1762198437236!5m2!1sen!2sus" width="200" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe></li>
+                        <li><iframe src="https://www.google.com/maps/place/Verstera+Roofing/@30.588255,-97.73297,242401m/data=!3m2!1e3!4b1!4m6!3m5!1s0xe8378ec716b107:0xad97f3a517d3a80b!8m2!3d30.588255!4d-97.73297!16s%2Fg%2F11xfhkml7p?hl=en&entry=ttu&g_ep=EgoyMDI2MDMzMC4wIKXMDSoASAFQAw%3D%3D"></iframe></li>
                           <!--<li><a href="guadalupe-county.html">Guadalupe County</a></li>-->
                          <!-- <li><a href="bell-county.html">Bell County</a></li>-->
                          <!-- <li><a href="travis-county.html">Travis County</a></li>-->
